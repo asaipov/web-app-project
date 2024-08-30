35ad9198e0911d1f2c42e81cd45ed3f082b69b4f
asaipov <aipoxalexander@gmail.com>
2024-08-30 20:28:46 +0300

    feat: init i18next and add base config #7

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { AboutPage, App } from "./pages";
+import i18n from "./shared/i18n-config/i18n/i18nProvider";
 
 const root = document.getElementById("root");
 
@@ -21,4 +24,10 @@ const router = createBrowserRouter([
 ]);
 
 const container = createRoot(document.getElementById("root"));
-container.render(<RouterProvider router={router} />);
+container.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <RouterProvider router={router} />
+    </I18nextProvider>
+  </React.StrictMode>
+);
