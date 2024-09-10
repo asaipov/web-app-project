62938e4731539323a3670f005156f16e207c2236
asaipov <aipoxalexander@gmail.com>
2024-09-10 22:51:39 +0300

    feat: add theme switcher #27

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import "./shared/styles/nullstyle.scss";
 
+import { ConfigProvider, theme } from "antd";
+import { useUnit } from "effector-react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
@@ -7,12 +9,26 @@ import { RouterProvider } from "react-router-dom";
 
 import i18n from "./app/i18n-config/i18n/i18nProvider";
 import { router } from "./app/providers/routes";
+import { themeModel } from "./features/theme-switcher";
 
 const container = createRoot(document.getElementById("root"));
-container.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <RouterProvider router={router} />
-    </I18nextProvider>
-  </React.StrictMode>
-);
+
+const App = () => {
+  const isDarkTheme = useUnit(themeModel.$themeState);
+
+  return (
+    <React.StrictMode>
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkTheme ? theme.defaultAlgorithm : theme.darkAlgorithm,
+        }}
+      >
+        <I18nextProvider i18n={i18n}>
+          <RouterProvider router={router} />
+        </I18nextProvider>
+      </ConfigProvider>
+    </React.StrictMode>
+  );
+};
+
+container.render(<App />);
--- src/pages/Layout/ui/Layout.tsx
+++ src/pages/Layout/ui/Layout.tsx
@@ -1,15 +1,17 @@
+import { ThemeSwitcher } from "@src/features/theme-switcher";
 import { HeaderLayout } from "@src/widgets/Header";
+import { Layout as AppLayout } from "antd";
 import { Outlet } from "react-router-dom";
 
 import * as styles from "./styles.module.scss";
 
 export const Layout = () => {
   return (
-    <>
-      <HeaderLayout />
+    <AppLayout className={styles.layout_style}>
+      <HeaderLayout propsComponent={<ThemeSwitcher />} />
       <div className={styles.wrapper}>
         <Outlet />
       </div>
-    </>
+    </AppLayout>
   );
 };
